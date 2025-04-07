865e2905e99b4dedbfe263033e9593290037d6dd
Vedant Warhekar <vwarhekar444@gmail.com>
2025-04-07 14:21:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Learn about redux toolkit!
+      </h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
